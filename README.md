fcd45d2b55dab413da4d102df693178a18cc63c6
Pranith Gunda <pranith0527@yahoo.com>
2024-03-30 23:06:11 -0400

    Completed function collectEmployees and moved index.html to root

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Employee Payroll Tracker Starter Code
+
+## Description
+
+Employee Payroll tracker application is built to track employee annual salaries and enables employer to budget the funds for employees accordingly.
+The application takes employee information including salary and dynamically generates the table to display employee information. Console logs the average salary of
+employees and radomly chosen employee as a winner.
+
+The motivation behind this project is to add user interaction with the application using Javascript. Objects, Arrays and Primitives were used to capture employee information
+and perform calculations for average salary. DOM manipulation was used to generate dynamic table to present employee information.
+
+## Screenshot
+
+coming soon
+
+## Link to deployed site
+
+coming soon
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,142 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+// Function returns an array of employee objects
+const collectEmployees = function() {
+
+  //Define employee object
+
+  let employees = {};
+
+  //Define employees array
+
+  let employeesArray = [];
+
+  //prompt user for input. prompt method displays a dialog box that prompts the user for input
+
+  let firstName =  prompt("Enter First Name:");
+  let lastName  =  prompt("Enter Last Name:");
+  let salary    =  prompt("Enter Salary:");
+
+  // Check if salary is number, else default to 0
+
+  salary =  isNaN(salary) ? 0 : salary;
+
+  // Insert into object and push to array if above attributes are not null else set them to null
+
+  if(firstName != "" && lastName != "" && salary != ""){
+    employees['firstName'] = firstName;
+    employees['lastName']  = lastName;
+    employees['salary']    = salary;
+    employeesArray.push(employees);
+  }else{
+    firstName = "";
+    lastName  = "";
+    salary    = "";
+  }
+
+  // Confirm if user wants to add another employee, clicking 'ok' returns true and 'cancel' returns false
+
+  let addAnotherEmployee = confirm("Do you want to add another employee ?");
+
+  while(addAnotherEmployee){
+
+  firstName =  prompt("Enter First Name:");
+  lastName  =  prompt("Enter Last Name:");
+  salary    =  prompt("Enter Salary:");
+
+  salary =  isNaN(salary) ? 0 : salary;
+
+  // Insert into object and push to array if above attributes are not null else set them to null
+
+  if(firstName != "" && lastName != "" && salary != ""){
+
+    // Instantiate object to not override previously entered data
+    let employees = {};
+
+    employees['firstName'] = firstName;
+    employees['lastName']  = lastName;
+    employees['salary']    = salary;
+
+    employeesArray.push(employees);
+  }else{
+    firstName = "";
+    lastName  = "";
+    salary    = "";
+  }
+    addAnotherEmployee = confirm("Do you want to add another employee ?");
+  }
+  return employeesArray;
+}
+
+// Display the average salary among all the employees
+const displayAverageSalary = (employeesArray) => {
+}
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+
+  
+}
+
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
